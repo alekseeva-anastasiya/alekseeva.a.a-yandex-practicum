a872f23345e83ff1c51ed0c28e15f29406c574ed
Alekseeva Anastasiya <takaya.1yaodna@yandex.ru>
2023-09-30 07:03:29 +0100

    First commit

---
+++ configuration.py
@@ -0,0 +1,3 @@
+URL = "https://c5e64a64-ae45-4945-8b18-04e59692b124.serverhub.praktikum-services.ru"
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_BY_TRACK_PATH = "/api/v1/orders/track?t="
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Nastya",
+    "lastName": "Alekseeva",
+    "address": "Moscow, 142 apt.",
+    "metroStation": 4,
+    "phone": "+7 985 355 55 55",
+    "rentTime": 5,
+    "deliveryDate": "2023-09-30",
+    "comment": "Irina, come back to Moscow",
+    "color": [
+        "BLACK"
+    ]
+}
